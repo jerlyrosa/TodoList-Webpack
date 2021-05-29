ef02eec4f14e7347786b42c496dfc9a7d16fe837
sr-jerly <jerlymigueldelarosah@gmail.com>
2021-05-29 15:10:59 -0400

    Estrctura base

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const HtmlWebPack    = require('html-webpack-plugin');
+const MiniCssExtract = require('mini-css-extract-plugin');
+
+module.exports = {
+
+    mode: "development",
+    //LIMPIANDO Y CREANDO BUILD
+    output:{
+        clean: true
+    },
+
+    //REGLAS DEL BUILD
+    module: {
+        rules: [
+            {
+                test:/\.html$/,
+                loader: 'html-loader',
+                options:{
+                    sources: false,
+                },
+
+            },
+            {
+                test:/\.css$/,
+                exclude: /.styles.css$/,
+                use: [
+                    'style-loader', 
+                    'css-loader'
+                ]
+
+                },
+                {
+                    test:/.styles.css$/,
+                    use: [ MiniCssExtract.loader, 'css-loader'] ,
+
+                },{
+                    test:/\.(png|jpe?g|gif)$/,
+                    loader: 'file-loader',
+                }
+        ]
+    },
+ 
+    //Cofiguracion del pluginn
+
+      plugins:[
+        new HtmlWebPack({
+            title: 'WebPack App',
+            template: './src/index.html',
+            filename: './index.html'//OPCIONAL
+        }),
+        new MiniCssExtract ({
+            filename: '[name].css'
+        }),
+    ]
+
+}
